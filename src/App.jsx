e6e2cccf1328bbc866481d3e4a06b1b4b7ff1e67
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2023-11-15 15:22:04 +0530

    add: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+// import { useState } from 'react'
 import './App.css'
+import { Sidebar } from './components/sidebar/Sidebar'
 
 function App() {
+  // const [step,setStep] = useState(1);
 
   return (
     <>
+   <Sidebar/>
     </>
   )
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+import "./sidebar.css";
+
+const steps = [
+  { stepNum: 1, stepTitle: "YOUR INFO" },
+  { stepNum: 2, stepTitle: "SELECT PLAN" },
+  { stepNum: 3, stepTitle: "ADD-ONS" },
+  { stepNum: 4, stepTitle: "SUMMARY" },
+];
+
+export const Sidebar = () => {
+  return (
+    <>
+      <div className='sb-container flex-col'>
+   
+        {steps.map(({ stepNum, stepTitle }) => (
+          <div className='step-container flex' key={stepNum}>
+            <div className='step-num flex flex-center' >{stepNum}</div>
+            <div className='step-div'>
+              <div className='step-number'>STEP {stepNum}</div>
+              <div className='step-title'>{stepTitle}</div>
+            </div>
+          </div>
+        ))}
+  
+      </div>
+    </>
+  );
+};
