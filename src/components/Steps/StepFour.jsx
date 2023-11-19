573180471a96399e115490417cbc1eaabfe69b1c
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2023-11-19 16:19:51 +0530

    wip: step4

--- src/components/Steps/StepFour.jsx
+++ src/components/Steps/StepFour.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
-export const StepFour = ({ setStep }) => {
+export const StepFour = ({ setStep, accDetails, detailsDispatch }) => {
   return (
     <div className='flex flex-col flex-grow card-padding'>
       <h1 className='text-mb text-bold m-0'>Finishing up</h1>
       <div className='text-cg text-reg mt-10'>
         Double-check everything looks OK before confirming.
+      </div>
+
+      <div className='summary-div'>
+        <div className='flex flex-center'>
+          <div>
+            <div className='text-mb text-bold'>
+              {accDetails.plan.title}({accDetails.period})
+            </div>
+            <div className='underline-text text-cg'>Change</div>
+          </div>
+          <div className='text-mb text-bold ml-auto'>
+            {accDetails.plan.price}
+          </div>
+        </div>
+        <div className='hr-break' />
       </div>
 
       <div className='flex mt-auto flex-center'>
