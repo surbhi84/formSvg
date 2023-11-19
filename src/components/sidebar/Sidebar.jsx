0d13551d7a55931d51e9af6bd3e5039df616bea6
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2023-11-19 13:24:07 +0530

    feat: sidebar update

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,21 +7,26 @@ const steps = [
   { stepNum: 4, stepTitle: "SUMMARY" },
 ];
 
-export const Sidebar = () => {
+export const Sidebar = ({ step, setStep }) => {
   return (
     <>
       <div className='sb-container flex-col'>
-   
         {steps.map(({ stepNum, stepTitle }) => (
           <div className='step-container flex' key={stepNum}>
-            <div className='step-num flex flex-center' >{stepNum}</div>
+            <div
+              className={`${
+                step === stepNum ? "step-num-active" : "step-num"
+              }  flex flex-center`}
+              onClick={() => setStep(stepNum)}
+            >
+              {stepNum}
+            </div>
             <div className='step-div'>
               <div className='step-number'>STEP {stepNum}</div>
               <div className='step-title text-med'>{stepTitle}</div>
             </div>
           </div>
         ))}
-  
       </div>
     </>
   );
