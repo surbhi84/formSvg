39f2b1102d127271cc82541d651cf08fcf7f01e8
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2023-11-20 22:52:56 +0530

    add: field required error message

--- src/components/Steps/StepOne.jsx
+++ src/components/Steps/StepOne.jsx
@@ -1,8 +1,16 @@
 import "./steps.css";
 import { EDITDETAILS } from "../../hooks/dataReducer/types";
+import { useState } from "react";
 
 export const StepOne = ({ setStep, accDetails, detailsDispatch }) => {
+  const [showError, setShowError] = useState({
+    userName: false,
+    email: false,
+    phone: false,
+  });
+
   const handleChange = (e) => {
+    setShowError((p) => ({ ...p, [e.target.name]: false }));
     detailsDispatch({
       type: EDITDETAILS,
       payload: { [e.target.name]: e.target.value },
@@ -16,11 +24,16 @@ export const StepOne = ({ setStep, accDetails, detailsDispatch }) => {
         Please provide your name, email address, and phone number.
       </div>
 
-      <form className='form' onSubmit={() => {}}>
-        <div className='flex-col'>
+      <div className='form' onSubmit={() => {}}>
+        <div className='flex-col pos-rel'>
           <label className='text-mb text-med'>Name</label>
+          {showError.userName && (
+            <div className='err-msg'> This field is required</div>
+          )}
           <input
-            className='input-field text-med'
+            className={`${
+              showError.userName && "input-error"
+            } input-field text-med`}
             type='name'
             name='userName'
             placeholder='e.g.Stephen King'
@@ -29,11 +42,15 @@ export const StepOne = ({ setStep, accDetails, detailsDispatch }) => {
           />
         </div>
 
-        <div className='flex-col'>
+        <div className='flex-col  pos-rel'>
           <label className='text-mb text-med'>Email Address</label>
-
+          {showError.email && (
+            <div className='err-msg'> This field is required</div>
+          )}
           <input
-            className='input-field text-med'
+            className={`${
+              showError.email && "input-error"
+            } input-field text-med`}
             type='email'
             name='email'
             placeholder='e.g.stephenking@gmail.com'
@@ -43,11 +60,15 @@ export const StepOne = ({ setStep, accDetails, detailsDispatch }) => {
         </div>
 
         {/* {errors.email && touched.email && errors.email} */}
-        <div className='flex-col'>
+        <div className='flex-col  pos-rel'>
           <label className='text-mb text-med'>Phone Number</label>
-
+          {showError.phone && (
+            <div className='err-msg'> This field is required</div>
+          )}
           <input
-            className='input-field text-med'
+            className={`${
+              showError.phone && "input-error"
+            } input-field text-med`}
             type='tel'
             name='phone'
             placeholder='e.g.+1234567890'
@@ -60,17 +81,26 @@ export const StepOne = ({ setStep, accDetails, detailsDispatch }) => {
 
         <div className='flex mt-auto dis-none'>
           <button
-            type='submit'
             className='btn-primary mt-auto ml-auto'
-            // disabled={}
+            // disabled={
+            //   accDetails.userName === "" ||
+            //   accDetails.email === "" ||
+            //   accDetails.phone === ""
+            // }
             onClick={() => {
-              setStep((p) => p + 1);
+              if (accDetails.userName === "")
+                setShowError((p) => ({ ...p, userName: true }));
+              if (accDetails.email === "")
+                setShowError((p) => ({ ...p, email: true }));
+              if (accDetails.phone === "") {
+                setShowError((p) => ({ ...p, phone: true }));
+              } else setStep((p) => p + 1);
             }}
           >
             Next Step
           </button>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
